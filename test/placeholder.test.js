7c22c534d200c2cb2cd7312706b63f29adf4118b
Tyler Ball <tball@trapeze.com>
2012-05-30 14:05:13 -0400

    WIP
    Added AUTHORS file, README formatting and package.json cleanup
    
    moving stuff around
    
    moving stuff around
    
    Update modules

--- test/placeholder.test.js
+++ test/placeholder.test.js
@@ -1,6 +1,6 @@
 var should = chai.should();
 
-suite('Placeholder', function () {
+suite('Placeholder Globals', function () {
     Trapeze.ph = new Trapeze.Placeholder();
     var ph = Trapeze.ph;
 
@@ -12,20 +12,52 @@ suite('Placeholder', function () {
         Trapeze.Placeholder.should.be.a('function');
     });
 
-    test('Placeholder instance "ph" should be instance of Placeholder', function() {
+    test('Placeholder instance "ph" should be instance of Placeholder', 
+    function() {
         Trapeze.Placeholder.should.be.a('function');
-        ph.should.be.an.instanceof(Trapeze.Placeholder);
+        ph.should.be.instanceOf(Trapeze.Placeholder);
     });
 });
 
-suite('Old Browser Tests', function () {
-    $(function () {
+$(function () {
+    suite('Placeholder Active', function () {
         Modernizr.input.placeholder = false;
         Trapeze.old = new Trapeze.Placeholder();
-        var old = Trapeze.old;
+
+        var old = Trapeze.old,
+            val = 'Placeholder',
+            iclass = old.config.classnames.state_inactive,
+            aclass = old.config.classnames.state_active;
 
         test('Elements with Placeholders should be selected', function () {
             old.$elements.should.have.length(1);
+        });
+
+        test('Element should contain value of placeholder attribute', 
+        function () {
+            old.$elements.attr('value').should.eql(val);
+        });
+
+        test('Element should have inactive class', function () {
+            var iclass = old.config.classnames.state_inactive;
+
+            old.$elements.hasClass(iclass).should.be.true;
+        });
+
+        test('Element should have empty value and active class when focused', 
+        function () {
+            old.$elements.focus();
+
+            old.$elements.val().should.eql('');
+            old.$elements.hasClass(aclass).should.be.true;
+        });
+
+        test('Element should have original value and inactive class when blurred', 
+        function () {
+            old.$elements.blur();
+
+            old.$elements.val().should.eql(val);
+            old.$elements.hasClass(iclass).should.be.true;
         });
     });
 });
