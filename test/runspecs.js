0446eae29a509edfbf7826061630a9eef25baba2
Tyler Ball <tball@trapeze.com>
2012-08-10 16:52:47 -0400

    remove unused file

--- test/runspecs.js
+++
@@ -1,7 +0,0 @@
-var jsdom = require('jsdom').jsdom,
-    placeholder = require("../dist/js/placeholder.js"),
-    window = jsdom(placeholder).createWindow(),
-    $ = require("jquery");
-
-require("chai");
-require("./placeholder.test.js");
